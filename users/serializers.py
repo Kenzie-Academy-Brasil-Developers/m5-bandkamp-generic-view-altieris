83a052f0d1269f8fffe9ff4c0f6000bd2660b069
unknown <altmsf15@gmail.com>
2023-02-24 10:07:42 -0300

    model de user refatorada

--- users/serializers.py
+++ users/serializers.py
@@ -3,8 +3,7 @@ from rest_framework.validators import UniqueValidator
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
         validators=[
             UniqueValidator(
@@ -17,17 +16,25 @@ class UserSerializer(serializers.Serializer):
         validators=[UniqueValidator(queryset=User.objects.all())],
     )
     password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
     is_superuser = serializers.BooleanField(read_only=True)
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_superuser(**validated_data)
-
+    
     def update(self, instance: User, validated_data: dict) -> User:
         for key, value in validated_data.items():
             setattr(instance, key, value)
+        
+        try:
+            instance.set_password(validated_data['password'])
+            instance.save()
+        except KeyError:
+            pass
 
         instance.save()
 
         return instance
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'is_superuser']
