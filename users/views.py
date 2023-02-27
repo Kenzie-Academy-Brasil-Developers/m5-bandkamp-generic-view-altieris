761a0c944e868be5db07544cf3394105207dd5f6
unknown <altmsf15@gmail.com>
2023-02-27 14:39:29 -0300

    refatorando para generic view

--- users/views.py
+++ users/views.py
@@ -1,57 +1,20 @@
 from rest_framework.views import APIView, Request, Response, status
-
-from utils.common_views import PostView
-from utils.details_views import GetDetailView, PatchDetailView
+from users.permissions import IsAccountOwner
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from django.shortcuts import get_object_or_404
-from .permissions import IsAccountOwner
+from rest_framework import generics
 
 
-class UserView(PostView):
+class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()        
-    serializer = UserSerializer
+    serializer_class = UserSerializer
 
 
-class UserDetailView(GetDetailView, PatchDetailView):
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
 
     queryset = User.objects.all()        
-    serializer = UserSerializer
-
-    def delete(self, request: Request, pk: int) -> Response:
-        """
-        Deleçao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        user.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# from rest_framework.views import APIView, Request, Response, status
-
-# from utils.common_views import PostView
-# from .models import User
-# from rest_framework_simplejwt.authentication import JWTAuthentication
-# from .serializers import UserSerializer
-# from .permissions import IsAccountOwner
-# from utils.details_views import OnlyDeleteDetailView, OnlyGetDetailView, OnlyPatchDetailView
-
-
-# class UserView(PostView):
-#     queryset = User.objects.all()        
-#     serializer = UserSerializer
-
-
-# class UserDetailView(OnlyGetDetailView, OnlyPatchDetailView, OnlyDeleteDetailView):
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsAccountOwner]
-    
-#     view_queryset = User.objects.all()
-#     view_serializer = UserSerializer
+    serializer_class = UserSerializer
